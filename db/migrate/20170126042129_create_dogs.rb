ff6340be1f9ec661aeb90d534ce560e10fc9d7ec
mr-jungchoi <mr.jungchoi@gmail.com>
2017-01-27 14:43:53 -0500

    Fix typo for dogs migration

--- db/migrate/20170126042129_create_dogs.rb
+++ db/migrate/20170126042129_create_dogs.rb
@@ -8,7 +8,7 @@ class CreateDogs < ActiveRecord::Migration[5.0]
       t.string   :breed
       t.text     :bio
       t.string   :avatar
-      t.reference :user, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps(null: false)
     end
